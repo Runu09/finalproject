1cbdbe5a0abeb5a727b6c33e3ad22ee8d9508744
Runu09 <choesc1009@kakao.com>
2022-06-11 15:59:52 +0900

    add to index

---
+++ finalproj/src/main/java/com/onair/proj/FinalprojApplication.java
@@ -0,0 +1,13 @@
+package com.onair.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojApplication.class, args);
+	}
+
+}
