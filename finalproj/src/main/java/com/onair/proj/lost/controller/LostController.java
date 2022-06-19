160d1a9a59545d45974957cd34c365f70c2a785f
kcat2201 <kcat2201@gmail.com>
2022-06-20 00:30:14 +0900

    유실물 상세보기 - 댓글 목록, 댓글 달기 추가

---
+++ finalproj/src/main/java/com/onair/proj/lost/controller/LostController.java
@@ -0,0 +1,13 @@
+package com.onair.proj.lost.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lost")
+public class LostController {
+	@RequestMapping("/detail.do")
+	public void detail() {
+		
+	}
+}
