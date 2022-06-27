4b27413aed914214dc53b6c1d0d8d66255377893
lsb1211 <105209968+lsb1211@users.noreply.github.com>
2022-06-27 17:17:02 +0900

    lsb

--- finalproj/src/main/java/com/onair/proj/configuration/MvcConfiguration.java
+++ finalproj/src/main/java/com/onair/proj/configuration/MvcConfiguration.java
@@ -14,7 +14,8 @@ public class MvcConfiguration implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoginInterceptor())
-		.addPathPatterns("/lost/write.do");
+		.addPathPatterns("/lost/write.do","/member/editMem.do","/member/editPwd.do",
+				"/member/outMem.do","/mypage/mypageMain.do","/mypage/bookings.do");
 		
 		/*
 		 * registry.addInterceptor(new AdminLoginInterceptor())
